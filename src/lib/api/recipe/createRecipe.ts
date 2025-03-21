75d202e9d0152b92aadbe81711ce8000bc3bb428
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2025-03-21 12:04:51 -0700

    able to create a recipe

---
+++ src/lib/api/recipe/createRecipe.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { getAuthSession } from '../../Auth';
+import { baseUrl } from '../../../config';
+import { AuthSessionInterface } from '../../../types/authInterface';
+import { RecipeInterface } from '../../../types/recipe';
+
+/**
+ * create a new recipe with @param recipe for the current signed-in user.
+ *
+ * @param {RecipeInterface} recipe
+ * @return {Promise<any>}
+ */
+
+
+const createRecipe = (recipe: RecipeInterface) => {
+    return new Promise((resolve, reject) => {
+        getAuthSession()
+            .then((session: AuthSessionInterface ) => {
+                const { id, accessToken } = session;
+                const params = { recipe }
+                const reqBody = {
+                    headers: { Authorization: `Bearer ${accessToken}` },
+                }
+
+                axios
+                    .post(
+                        `${baseUrl}/recipe/${id}/create`,
+                        params,
+                        reqBody
+                    )
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(JSON.stringify(err)))
+            })
+            .catch((err) =>
+                reject(`getAuthSession::error - ${JSON.stringify(err)}`)
+            )
+    })
+}
+
+export default createRecipe
+
